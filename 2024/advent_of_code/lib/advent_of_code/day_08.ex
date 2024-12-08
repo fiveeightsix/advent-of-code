a927962c7bd9ca60e9aa999289922aea4771c474
Karl <mail@karlinglis.net>
2024-12-08 23:16:39 +0000

    Day 08 part 1 complete!

--- 2024/advent_of_code/lib/advent_of_code/day_08.ex
+++ 2024/advent_of_code/lib/advent_of_code/day_08.ex
@@ -28,16 +28,31 @@ defmodule AdventOfCode.Day08 do
     end)
   end
 
+  def antinodes({ax, ay}, {bx, by}) do
+    {dx, dy} = {bx - ax, by - ay}
+    
+    [{ax - dx, ay - dy}, {bx + dx, by + dy}]
+  end
+
   def combinations(0, _), do: [[]]
   def combinations(_, []), do: []
   def combinations(k, [head|tail]) do
     (for l <- combinations(k - 1, tail), do: [head | l]) ++ combinations(k, tail)
   end
+
+  def is_outside_map({x, y}, width, height) do
+    x < 0 or x >= width or y < 0 or y >= height
+  end
   
   def part1(input) do
-    {map, _width, _height} = make_map(input)
+    {map, width, height} = make_map(input)
 
     locate_antennas(map)
-    |> Enum.map(fn {k, v} -> {k, combinations(2, v)} end)
+    |> Enum.map(fn {_, v} -> combinations(2, v) end)
+    |> Enum.flat_map(fn antenna_pair -> antenna_pair end)
+    |> Enum.flat_map(fn [a, b] -> antinodes(a, b) end)
+    |> Enum.reject(fn antinode -> is_outside_map(antinode, width, height) end)
+    |> Enum.uniq()
+    |> Enum.count()
   end
 end
