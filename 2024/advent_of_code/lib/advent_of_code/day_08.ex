60bcfa3ede3f234917026082c9f6bdc27d63ee07
Karl <mail@karlinglis.net>
2024-12-09 00:17:54 +0000

    Day 08 part 2 complete

--- 2024/advent_of_code/lib/advent_of_code/day_08.ex
+++ 2024/advent_of_code/lib/advent_of_code/day_08.ex
@@ -28,19 +28,19 @@ defmodule AdventOfCode.Day08 do
     end)
   end
 
-  def antinodes({ax, ay}, {bx, by}) do
-    {dx, dy} = {bx - ax, by - ay}
-    
-    [{ax - dx, ay - dy}, {bx + dx, by + dy}]
-  end
-
   def combinations(0, _), do: [[]]
   def combinations(_, []), do: []
   def combinations(k, [head|tail]) do
     (for l <- combinations(k - 1, tail), do: [head | l]) ++ combinations(k, tail)
   end
 
-  def is_outside_map({x, y}, width, height) do
+  def antinodes({ax, ay}, {bx, by}) do
+    {dx, dy} = {bx - ax, by - ay}
+    
+    [{ax - dx, ay - dy}, {bx + dx, by + dy}]
+  end
+
+  def outside_map?({x, y}, width, height) do
     x < 0 or x >= width or y < 0 or y >= height
   end
   
@@ -51,7 +51,38 @@ defmodule AdventOfCode.Day08 do
     |> Enum.map(fn {_, v} -> combinations(2, v) end)
     |> Enum.flat_map(fn antenna_pair -> antenna_pair end)
     |> Enum.flat_map(fn [a, b] -> antinodes(a, b) end)
-    |> Enum.reject(fn antinode -> is_outside_map(antinode, width, height) end)
+    |> Enum.reject(fn antinode -> outside_map?(antinode, width, height) end)
+    |> Enum.uniq()
+    |> Enum.count()
+  end
+
+  defguard is_inside_map(x, y, width, height) when x >= 0 and x < width and y >= 0 and y < height
+  
+  def antinodes_2_add({x, y} = a, {dx, dy} = d, width, height) when is_inside_map(x, y, width, height) do
+    [a | antinodes_2_add({x + dx, y + dy}, d, width, height)]
+  end
+
+  def antinodes_2_add(_, _, _, _), do: []
+
+  def antinodes_2_sub({x, y} = a, {dx, dy} = d, width, height) when is_inside_map(x, y, width, height) do
+    [a | antinodes_2_sub({x - dx, y - dy}, d, width, height)]
+  end
+
+  def antinodes_2_sub(_, _, _, _), do: []
+  
+  def antinodes_2({ax, ay} = a, {bx, by} = b, width, height) do
+    d = {bx - ax, by - ay}
+    
+    antinodes_2_sub(a, d, width, height) ++ antinodes_2_add(b, d, width, height)
+  end
+  
+  def part2(input) do
+    {map, width, height} = make_map(input)
+
+    locate_antennas(map)
+    |> Enum.map(fn {_, v} -> combinations(2, v) end)
+    |> Enum.flat_map(fn antenna_pair -> antenna_pair end)
+    |> Enum.flat_map(fn [a, b] -> antinodes_2(a, b, width, height) end)
     |> Enum.uniq()
     |> Enum.count()
   end
---
+++ 2024/advent_of_code/lib/mix/tasks/d08.p2.ex
@@ -0,0 +1,17 @@
+defmodule Mix.Tasks.D08.P2 do
+  use Mix.Task
+
+  import AdventOfCode.Day08
+
+  @shortdoc "Day 08 part 2"
+  def run(_args) do
+    path = Path.join(File.cwd!, "lib/input/day_08.txt")
+
+    case File.read(path) do
+      {:ok, input} -> input
+          |> part2
+          |> IO.inspect(label: "Part 2 results")
+      {:error, message} -> IO.puts(message)
+    end
+  end
+end
