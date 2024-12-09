07b37a5503c1c4a39bff4699aab9b6efdf17c3d6
Karl <mail@karlinglis.net>
2024-12-09 20:41:22 +0000

    Day 09 part 1 complete

--- 2024/advent_of_code/lib/advent_of_code/day_09.ex
+++ 2024/advent_of_code/lib/advent_of_code/day_09.ex
@@ -1,4 +1,64 @@
 defmodule AdventOfCode.Day09 do
+  def disk_layout(input) do
+    layout =
+      input
+      |> String.split("", trim: true)
+      |> Enum.map(&Integer.parse/1)
+      |> Enum.reject(fn x -> x === :error end)
+      |> Enum.map(fn {i, _} -> i end)
+      |> Enum.with_index()
+      |> Enum.flat_map(fn {element, index} ->
+        case {element, index} do
+          {0, _} ->
+            []
+
+          {element, index} when rem(index, 2) === 0 ->
+            for _ <- 1..element, do: {:file, div(index, 2)}
+
+          {element, _} ->
+            for _ <- 1..element, do: {:space}
+        end
+      end)
+      |> Enum.with_index()
+      |> Map.new(fn {element, index} -> {index, element} end)
+
+    {layout, map_size(layout)}
+  end
+
+  def compact(layout, a, b) when a >= b, do: layout
+
+  def compact(layout, a, b) do
+    case {layout[a], layout[b]} do
+      {{:space}, {:space}} -> compact(layout, a, b - 1)
+      {{:space}, {:file, _}} -> compact(%{layout | a => layout[b], b => layout[a]}, a + 1, b - 1)
+      {{:file, _}, _} -> compact(layout, a + 1, b)
+    end
+  end
+
   def part1(input) do
+    {layout, size} = disk_layout(input)
+
+    compacted_layout = compact(layout, 0, size - 1)
+
+    0..(size - 1)
+    |> Enum.map(fn i -> {compacted_layout[i], i} end)
+    |> Enum.reduce(0, fn {block, i}, checksum ->
+      case block do
+        {:file, file_id} -> checksum + file_id * i
+        {:space} -> checksum
+      end
+    end)
+  end
+
+  def layout_to_string({layout, size}) do
+    0..(size - 1)
+    |> Enum.map(fn i -> layout[i] end)
+    |> Enum.map(fn x ->
+      case x do
+        {:file, file_id} -> Integer.to_string(file_id)
+        {:space} -> "."
+      end
+    end)
+    |> Enum.join()
   end
 end
--- 2024/advent_of_code/test/day_09_test.exs
+++ 2024/advent_of_code/test/day_09_test.exs
@@ -2,8 +2,38 @@ defmodule AdventOfCode.Day09Test do
   use ExUnit.Case
   import AdventOfCode.Day09
 
+  test "disk layout" do
+    input = """
+    2333133121414131402
+    """
+
+    expected = "00...111...2...333.44.5555.6666.777.888899"
+
+    actual = input |> disk_layout() |> layout_to_string()
+
+    assert actual === expected
+  end
+
+  test "compact" do
+    input = """
+    2333133121414131402
+    """
+
+    expected = "0099811188827773336446555566.............."
+
+    {layout, size} = disk_layout(input)
+
+    compacted_layout = compact(layout, 0, size - 1)
+
+    actual = layout_to_string({compacted_layout, size})
+
+    assert actual === expected
+  end
+
   test "part 1" do
-    input = "2333133121414131402"
+    input = """
+    2333133121414131402
+    """
 
     assert part1(input) === 1928
   end
