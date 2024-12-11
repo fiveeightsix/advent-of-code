40f9446d9b0ae851cb82bcf505b8c1ed182c29bc
Karl <mail@karlinglis.net>
2024-12-11 23:00:52 +0000

    Day 10: parse input, failing test

--- 2024/advent_of_code/lib/advent_of_code/day_10.ex
+++ 2024/advent_of_code/lib/advent_of_code/day_10.ex
@@ -1,5 +1,34 @@
 defmodule AdventOfCode.Day10 do
-  def part1(input) do
+  def make_map(input) do
+    parsed_input =
+      input
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn x ->
+        x
+        |> String.split("", trim: true)
+        |> Enum.map(&Integer.parse/1)
+        |> Enum.reject(fn x -> x === :error end)
+        |> Enum.map(fn {x, _} -> x end)
+      end)
 
+    max_x = Enum.count(parsed_input)
+    max_y = Enum.count(List.first(parsed_input))
+
+    map =
+      parsed_input
+      |> Enum.with_index(fn row, y -> {y, row} end)
+      |> Enum.reduce(Map.new(), fn {y, row}, map ->
+        row
+        |> Enum.with_index(fn height, x -> {x, height} end)
+        |> Enum.reduce(map, fn {x, height}, map ->
+          Map.put(map, {x, y}, height)
+        end)
+      end)
+      
+    {map, max_x, max_y}
+  end
+  
+  def part1(input) do
+    {map, max_x, max_y} = make_map(input)
   end
 end
--- 2024/advent_of_code/test/day_10_test.exs
+++ 2024/advent_of_code/test/day_10_test.exs
@@ -2,4 +2,18 @@ defmodule AdventOfCode.Day10Test do
   use ExUnit.Case
   import AdventOfCode.Day10
 
+  test "Part 1" do
+    input = """
+    89010123
+    78121874
+    87430965
+    96549874
+    45678903
+    32019012
+    01329801
+    10456732
+    """
+
+    assert part1(input) === 36
+  end
 end
