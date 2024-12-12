1df41612f68b1d71ad933c564e2a535c4b06341c
Karl <mail@karlinglis.net>
2024-12-12 01:39:35 +0000

    Day 10 part 1 complete

--- 2024/advent_of_code/lib/advent_of_code/day_10.ex
+++ 2024/advent_of_code/lib/advent_of_code/day_10.ex
@@ -1,34 +1,61 @@
 defmodule AdventOfCode.Day10 do
   def make_map(input) do
-    parsed_input =
+    {map, trailheads, max_x, x, y} =
       input
-      |> String.split("\n", trim: true)
-      |> Enum.map(fn x ->
-        x
-        |> String.split("", trim: true)
-        |> Enum.map(&Integer.parse/1)
-        |> Enum.reject(fn x -> x === :error end)
-        |> Enum.map(fn {x, _} -> x end)
+      |> String.split("", trim: true)
+      |> Enum.reduce({Map.new(), [], 0, 0, 0}, fn c, {map, trailheads, max_x, x, y} ->
+        case Integer.parse(c) do
+          {height, _} when height === 0 ->
+            {Map.put(map, {x, y}, height), [{x, y} | trailheads], max(max_x, x + 1), x + 1, y}
+
+          {height, _} ->
+            {Map.put(map, {x, y}, height), trailheads, max(max_x, x + 1), x + 1, y}
+
+          :error ->
+            {map, trailheads, max_x, 0, y + 1}
+        end
       end)
 
-    max_x = Enum.count(parsed_input)
-    max_y = Enum.count(List.first(parsed_input))
-
-    map =
-      parsed_input
-      |> Enum.with_index(fn row, y -> {y, row} end)
-      |> Enum.reduce(Map.new(), fn {y, row}, map ->
-        row
-        |> Enum.with_index(fn height, x -> {x, height} end)
-        |> Enum.reduce(map, fn {x, height}, map ->
-          Map.put(map, {x, y}, height)
-        end)
-      end)
-      
-    {map, max_x, max_y}
+    {map, trailheads, max_x, y}
   end
-  
+
+  def get_neighbours(map, {x, y}) do
+    [{x, y - 1}, {x + 1, y}, {x, y + 1}, {x - 1, y}]
+    |> Enum.map(fn point -> {point, Map.get(map, point)} end)
+    |> Enum.reject(fn {point, height} -> height === nil end)
+  end
+
+  def climb(point, map), do: climb_rec(map, MapSet.new(), [{point, 0}], 0)
+
+  def climb_rec(_, _, [], score), do: score
+
+  def climb_rec(map, visited, queue, score) do
+    [{point, height} | remaining] = queue
+
+    new_points =
+      map
+      |> get_neighbours(point)
+      |> Enum.reject(fn {neighbour_point, _} ->
+        MapSet.member?(visited, neighbour_point) === true
+      end)
+      |> Enum.reject(fn {_, neighbour_height} -> neighbour_height !== height + 1 end)
+
+    climb_rec(
+      map,
+      MapSet.put(visited, point),
+      new_points ++ remaining,
+      case height do
+        9 -> score + 1
+        _ -> score
+      end
+    )
+  end
+
   def part1(input) do
-    {map, max_x, max_y} = make_map(input)
+    {map, trailheads, width, height} = make_map(input)
+
+    trailheads
+    |> Enum.map(fn trailhead -> climb(trailhead, map) end)
+    |> Enum.sum()
   end
 end
--- 2024/advent_of_code/test/day_10_test.exs
+++ 2024/advent_of_code/test/day_10_test.exs
@@ -2,6 +2,44 @@ defmodule AdventOfCode.Day10Test do
   use ExUnit.Case
   import AdventOfCode.Day10
 
+  test "Make map - finds trail heads" do
+    input = """
+    89010123
+    78121874
+    87430965
+    96549874
+    45678903
+    32019012
+    01329801
+    10456732
+    """
+
+    expected_trailheads = [{2, 0}, {4, 0}, {4, 2}, {6, 4}, {2, 5}, {5, 5}, {0, 6}, {6, 6}, {1, 7}]
+
+    {_, actual_trailheads, width, height} = make_map(input)
+
+    assert Enum.sort(actual_trailheads) === Enum.sort(expected_trailheads)
+    assert width === 8
+    assert height === 8
+  end
+
+  test "Climb" do
+    input = """
+    89010123
+    78121874
+    87430965
+    96549874
+    45678903
+    32019012
+    01329801
+    10456732
+    """
+
+    {map, _, _, _} = make_map(input)
+
+    assert climb({2, 0}, map) === 5
+  end
+
   test "Part 1" do
     input = """
     89010123
