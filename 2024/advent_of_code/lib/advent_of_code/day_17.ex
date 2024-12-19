875e949143ffe61b2778704c7d708755d3d62fea
Karl <mail@karlinglis.net>
2024-12-19 22:57:22 +0000

    Day 17 part 1 complete

--- 2024/advent_of_code/lib/advent_of_code/day_17.ex
+++ 2024/advent_of_code/lib/advent_of_code/day_17.ex
@@ -7,28 +7,25 @@ defmodule AdventOfCode.Day17 do
       {first, second}
     end
     
-    def register(name) do
+    def register(name, label) do
       ignore(string("Register "))
-      |> string(name)
+      |> ignore(string(name))
       |> ignore(string(": "))
       |> integer(min: 1)
-      |> reduce({:to_2_tuple, []})
+      |> unwrap_and_tag(label)
     end
 
     def registers() do
-      eventually(register("A"))
-      |> eventually(register("B"))
-      |> eventually(register("C"))
+      eventually(register("A", :a))
+      |> eventually(register("B", :b))
+      |> eventually(register("C", :c))
       |> reduce({Map, :new, []})
       |> unwrap_and_tag(:registers)
     end
     
     def instruction() do
       integer(min: 1, max: 2)
-      |> ignore(string(","))
-      |> integer(min: 1, max: 2)
       |> optional(ignore(string(",")))
-      |> reduce({:to_2_tuple, []})
     end
     
     def program() do
@@ -53,56 +50,140 @@ defmodule AdventOfCode.Day17 do
       1 -> 1
       2 -> 2
       3 -> 3
-      4 -> Map.get(registers, "A")
-      5 -> Map.get(registers, "B")
-      6 -> Map.get(registers, "C")
+      4 -> registers.a
+      5 -> registers.b
+      6 -> registers.c
     end
   end
 
+  @doc """
+  Performs division. The numerator is the value in the A register. The denominator is
+  found by raising 2 to the power of the instruction's combo operand. The result of
+  the division operation is truncated to an integer and then written to the A register.
+  """
   def instruction_adv(registers, operand) do
+    numerator = registers.a
+    denominator = Integer.pow(2, get_combo_value(registers, operand))
+    value = Integer.floor_div(numerator, denominator)
 
+    {%{registers | a: value}, :no_output, :no_jump}
   end
 
+  @doc """
+  Calculates the bitwise XOR of register B and the instruction's literal operand,
+  then stores the result in register B.
+  """
   def instruction_bxl(registers, operand) do
-    numerator = Map.get(registers, "A")
-    denominator = get_combo_value(registers, operand) ** 2
-    numerator / denominator 
+    value = Bitwise.bxor(registers.b, operand)
+
+    {%{registers | b: value}, :no_output, :no_jump}
   end
 
+  @doc """
+  Calculates the value of its combo operand modulo 8 (thereby keeping only its lowest
+  3 bits), then writes that value to the B register.
+  """
   def instruction_bst(registers, operand) do
     value = registers |> get_combo_value(operand) |> rem(8)
     
-    %{registers | "B" => value}
+    {%{registers | b: value}, :no_output, :no_jump}
   end
-  
+
+  @doc """
+  Does nothing if the A register is 0. However, if the A register is not zero, it jumps
+  by setting the instruction pointer to the value of its literal operand.
+  """
   def instruction_jnz(registers, operand) do
-
+    case registers.a do
+      0 -> {registers, :no_output, :no_jump}
+      _ -> {registers, :no_output, operand}
+    end
   end
 
-  def instruction_bxc(registers, operand) do
+  @doc """
+  Calculates the bitwise XOR of register B and register C, then stores the result in
+  register B. (For legacy reasons, this instruction reads an operand but ignores it.)
+  """
+  def instruction_bxc(registers, _) do
+    value = Bitwise.bxor(registers.b, registers.c)
 
+    {%{registers | b: value}, :no_output, :no_jump}
   end
 
+  @doc """
+  Calculates the value of its combo operand modulo 8, then outputs that value.
+  """
   def instruction_out(registers, operand) do
+    output = registers |> get_combo_value(operand) |> rem(8)
 
+    {registers, output, :no_jump}
   end
 
+  @doc """
+  Works exactly like the adv instruction except that the result is stored in the B
+  register. (The numerator is still read from the A register.)
+  """
   def instruction_bdv(registers, operand) do
+    numerator = registers.a
+    denominator = Integer.pow(2, get_combo_value(registers, operand))
+    value = Integer.floor_div(numerator, denominator)
 
+    {%{registers | b: value}, :no_output, :no_jump}
   end
 
+  @doc """
+  Works exactly like the adv instruction except that the result is stored in the C
+  register. (The numerator is still read from the A register.)
+  """
   def instruction_cdv(registers, operand) do
+    numerator = registers.a
+    denominator = Integer.pow(2, get_combo_value(registers, operand))
+    value = Integer.floor_div(numerator, denominator)
 
+    {%{registers | c: value}, :no_output, :no_jump}
   end
-  
-  # def run(program, pointer, registers, output) do
 
-  #   updated_registers = case     
-  # end
+  def run(program_map, pointer, _) when pointer >= map_size(program_map), do: []
+  
+  def run(program_map, pointer, registers) do
+    opcode = Map.get(program_map, pointer)
+    operand = Map.get(program_map, pointer + 1)
+
+    {updated_registers, output, jump} =
+      case opcode do
+        0 -> instruction_adv(registers, operand)
+        1 -> instruction_bxl(registers, operand)
+        2 -> instruction_bst(registers, operand)
+        3 -> instruction_jnz(registers, operand)
+        4 -> instruction_bxc(registers, operand)
+        5 -> instruction_out(registers, operand)
+        6 -> instruction_bdv(registers, operand)
+        7 -> instruction_cdv(registers, operand)
+      end
+
+    next_pointer =
+      case jump do
+        :no_jump -> pointer + 2
+        jump -> jump
+      end
+
+    case output do
+      :no_output -> run(program_map, next_pointer, updated_registers)
+      output -> [output] ++ run(program_map, next_pointer, updated_registers)
+    end
+  end
   
   def part1(input) do
     {_, result, _, _, _, _} = Part1Parser.computer(input)
 
-    result
+    [registers: registers, program: program] = result
+
+    program_map =
+      program
+      |> Enum.with_index(fn number, index -> {index, number} end)
+      |> Map.new()
+
+    run(program_map, 0, registers)
+    |> Enum.join(",")
   end
 end
--- 2024/advent_of_code/test/day_17_test.exs
+++ 2024/advent_of_code/test/day_17_test.exs
@@ -2,27 +2,76 @@ defmodule AdventOfCode.Day17Test do
   use ExUnit.Case
   import AdventOfCode.Day17
 
-  test "opcode 2: instruction bst" do
-    registers = %{"A" => 0, "B" => 0, "C" => 9}
-    result = instruction_bst(registers, 6)
+  test "opcode 1: instruction bxl" do
+    registers = %{a: 0, b: 29, c: 0}
+    {actual_registers, _, _} = instruction_bxl(registers, 7)
 
-    assert result === %{"A" => 0, "B" => 1, "C" => 9}
+    assert actual_registers === %{a: 0, b: 26, c: 0}
   end
   
-  test "part 1" do
+  test "opcode 2: instruction bst" do
+    registers = %{a: 0, b: 0, c: 9}
+    {actual_registers, _, _} = instruction_bst(registers, 6)
+
+    assert actual_registers === %{a: 0, b: 1, c: 9}
+  end
+
+  test "opcode 4: instruction bxc" do
+    registers = %{a: 0, b: 2024, c: 43690}
+    {actual_registers, _, _} = instruction_bxc(registers, 0)
+
+    assert actual_registers === %{a: 0, b: 44354, c: 43690}
+  end
+
+  test "opcode 3: instruction jnz - does nothing" do
+    registers = %{a: 0, b: 0, c: 0}
+    operand = 8
+    {_, _, jump} = instruction_jnz(registers, operand)
+
+    assert jump === :no_jump
+  end
+
+  test "opcode 3: instruction jnz - jump to operand" do
+    registers = %{a: 1, b: 0, c: 0}
+    operand = 8
+    {_, _, jump} = instruction_jnz(registers, operand)
+
+    assert jump === operand
+  end
+  
+  test "part 1: A: 10, Program: 5,0,5,1,5,4" do
     input = """
-    RRRRIICCFF
-    RRRRIICCCF
-    VVRRRCCFFF
-    VVRCCCJFFF
-    VVVVCJJCFE
-    VVIVCCJJEE
-    VVIIICJJEE
-    MIIIIIJJEE
-    MIIISIJEEE
-    MMMISSJEEE
+    Register A: 10
+    Register B: 0
+    Register C: 0
+
+    Program: 5,0,5,1,5,4
     """
 
-    assert part1(input) === 1930
+    assert part1(input) === "0,1,2"
+  end
+
+  test "part 1: A: 2024, the program 0,1,5,4,3,0" do
+    input = """
+    Register A: 2024
+    Register B: 0
+    Register C: 0
+
+    Program: 0,1,5,4,3,0
+    """
+
+    assert part1(input) === "4,2,5,6,7,7,7,7,3,1,0"
+  end
+    
+  test "part 1" do
+    input = """
+    Register A: 729
+    Register B: 0
+    Register C: 0
+
+    Program: 0,1,5,4,3,0
+    """
+
+    assert part1(input) === "4,6,3,5,6,3,5,2,1,0"
   end
 end
