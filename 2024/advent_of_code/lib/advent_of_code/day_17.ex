f6d1c082baf8e21eba05b4a1113de472bafd2f96
Karl <mail@karlinglis.net>
2024-12-19 17:46:56 +0000

    First instruction test working

--- 2024/advent_of_code/lib/advent_of_code/day_17.ex
+++ 2024/advent_of_code/lib/advent_of_code/day_17.ex
@@ -2,9 +2,9 @@ defmodule AdventOfCode.Day17 do
   defmodule Part1Parser.Helpers do
     import NimbleParsec
 
-    def to_register(parsed_values) do
-      [name, value | _] = parsed_values
-      {name, value}
+    def to_2_tuple(parsed_values) do
+      [first, second | _] = parsed_values
+      {first, second}
     end
     
     def register(name) do
@@ -12,19 +12,15 @@ defmodule AdventOfCode.Day17 do
       |> string(name)
       |> ignore(string(": "))
       |> integer(min: 1)
-      |> reduce({:to_register, []})
+      |> reduce({:to_2_tuple, []})
     end
 
     def registers() do
       eventually(register("A"))
       |> eventually(register("B"))
-      |> eventually(register("C"))      
-      |> tag(:registers)
-    end
-
-    def to_instruction(parsed_values) do
-      [opcode, operand | _] = parsed_values
-      {opcode, operand}
+      |> eventually(register("C"))
+      |> reduce({Map, :new, []})
+      |> unwrap_and_tag(:registers)
     end
     
     def instruction() do
@@ -32,7 +28,7 @@ defmodule AdventOfCode.Day17 do
       |> ignore(string(","))
       |> integer(min: 1, max: 2)
       |> optional(ignore(string(",")))
-      |> reduce({:to_instruction, []})
+      |> reduce({:to_2_tuple, []})
     end
     
     def program() do
@@ -47,11 +43,66 @@ defmodule AdventOfCode.Day17 do
     import Part1Parser.Helpers
 
     computer =  eventually(registers()) |> eventually(program())
-        
+    
     defparsec(:computer, computer)
   end
 
+  def get_combo_value(registers, operand) do
+    case operand do
+      0 -> 0
+      1 -> 1
+      2 -> 2
+      3 -> 3
+      4 -> Map.get(registers, "A")
+      5 -> Map.get(registers, "B")
+      6 -> Map.get(registers, "C")
+    end
+  end
+
+  def instruction_adv(registers, operand) do
+
+  end
+
+  def instruction_bxl(registers, operand) do
+    numerator = Map.get(registers, "A")
+    denominator = get_combo_value(registers, operand) ** 2
+    numerator / denominator 
+  end
+
+  def instruction_bst(registers, operand) do
+    value = registers |> get_combo_value(operand) |> rem(8)
+    
+    %{registers | "B" => value}
+  end
+  
+  def instruction_jnz(registers, operand) do
+
+  end
+
+  def instruction_bxc(registers, operand) do
+
+  end
+
+  def instruction_out(registers, operand) do
+
+  end
+
+  def instruction_bdv(registers, operand) do
+
+  end
+
+  def instruction_cdv(registers, operand) do
+
+  end
+  
+  # def run(program, pointer, registers, output) do
+
+  #   updated_registers = case     
+  # end
+  
   def part1(input) do
-    Part1Parser.computer(input)
+    {_, result, _, _, _, _} = Part1Parser.computer(input)
+
+    result
   end
 end
--- 2024/advent_of_code/test/day_17_test.exs
+++ 2024/advent_of_code/test/day_17_test.exs
@@ -2,6 +2,13 @@ defmodule AdventOfCode.Day17Test do
   use ExUnit.Case
   import AdventOfCode.Day17
 
+  test "opcode 2: instruction bst" do
+    registers = %{"A" => 0, "B" => 0, "C" => 9}
+    result = instruction_bst(registers, 6)
+
+    assert result === %{"A" => 0, "B" => 1, "C" => 9}
+  end
+  
   test "part 1" do
     input = """
     RRRRIICCFF
